30b34277fb5d8aa374e0fc0291c07ac09ac31427
Vendare <Marco.Griesel@gmx.de>
2026-09-20 04:41:28 +0200

    Made the thing work

--- aa-aoss-integration.js
+++ aa-aoss-integration.js
@@ -8,17 +8,22 @@ Hooks.once("ready", () => {
 });
 
 function registerHooksAOSS() {
-    Hooks.on("createChatMessage", async (msg) => callAnimation(msg));
+    Hooks.on("updateChatMessage", async (msg) => callAnimation(msg));
 }
 
 async function callAnimation(msg) {
-    const test = msg.system.test;
+    const test = msg.test ?? msg.system.test;
+    if (!test) { return; }
     const item = test.item;
-    const targets = test.targetTokens
+    if(!item) { return; }
+    const targets = test.targetTokens.map(t => { return { document:t }; });
+    if (!targets || targets.length === 0) { return; }
     const sourceToken = canvas.tokens.get(msg.speaker.token)
         ?? canvas.tokens.placeables.find(t => t.actor?.id === msg.speaker.actor);
 
-    console.log(`AA-AOSS-Integration | Play Animation of "${item.name}" for Actor ${sourceToken.actor.name} against ${targets.map(t => t.actor.name).join(",")} `);
-    await AutoAnimations.playAnimation(sourceToken, targets, item);
+    console.log(`AA-AOSS-Integration | Play Animation of "${item.name}" for Actor ${sourceToken.actor.name} against ${targets.map(t => t.document.actor.name).join(",")} `);
+    await AutomatedAnimations.playAnimation(sourceToken, item, {
+        targets: targets
+    });
 }
 
